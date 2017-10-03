93d2dcab449d0ee3f098d37e04d9c377c03a0d94
Joey Howlett <jh3226@columbia.edu>
2017-10-03 10:46:27 +0200

    added oxygen partial outgassing code

---
+++ outgassing_progs/outgassing_oxygen.c
@@ -0,0 +1,47 @@
+#include "sub_macros/find_asymptotes.c"
+#include "sub_macros/locate_rga_dat.c"
+#include "sub_macros/analyze_rga_dat.c"
+#include "sub_macros/analyze_rga_dat_new_twocol.c"
+#include "sub_macros/plot_outgassing_errors.c"
+#include "sub_macros/plot_outgassing_errors_first.c"
+#include "sub_macros/plot_outgassing_errors_oxygen.c"
+#include "sub_macros/plot_outgassing_errors_second.c"
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <TString>
+#include "TFile.h"
+#include "TSpectrum.h"
+#include "TVirtualFitter.h"
+#include "sub_macros/settings.h"
+
+gROOT->Macro("sub_macros/style.C");
+void outgassing_oxygen(TString fname, Double_t input_mass =28.0) {
+   find_asymptotes(fname, measurement_time, measurement_pressure, fiterr, n_measurements, t_0);
+   vector<TString> rga_dats = locate_rga_dat(measurement_time, n_measurements);
+   vector<Double_t> meanmass(n_measurements), meanmass_err(n_measurements), throughput(n_measurements), mass_fraction(n_measurements);
+   vector<Double_t> time_hrs(n_measurements);
+   cout << "RGA Measurement:\t" << "Mean Mass (AMU):" << endl;
+   for (int i = 0; i<n_measurements; i++) {
+      TString rga_filename = rga_dats[i];
+      means = analyze_rga_dat_new_twocol(rga_filename,input_mass);
+      meanmass[i] = means.mass_peak;
+      if (meanmass[i] == 0 && i>0) {
+        meanmass[i] = meanmass[i-1]; //// in case rga .dat doesn't exist
+        mass_fraction[i] = mass_fraction[i-1];
+        cout << mass_fraction[i] << endl;
+      }
+      else mass_fraction[i] = means.mass_fraction;
+      cout << rga_dats[i] << "\t\t" << meanmass[i] << endl;//"\t± "<< meanmass_err[i] <<endl;
+      Double_t mass = meanmass[i];
+      Double_t conductance = coeff*trans_prob*1./(area*sqrt(mass));
+      measurement_pressure[i] = measurement_pressure[i]*mass_fraction[i];
+      throughput[i] = conductance*measurement_pressure[i];
+      time_hrs[i] = ((measurement_time[i] - t_0)/3600.);
+      if (print_time_outgassing==1) cout << time_hrs[i] << "\t" << throughput[i] << endl;
+
+   }
+   if (plot_number <= 1) plot_outgassing_errors_first(n_measurements, time_hrs, throughput, area, fiterr);
+   if (plot_number >  1) plot_outgassing_errors_second(n_measurements, time_hrs, throughput, area, fiterr);
+}
